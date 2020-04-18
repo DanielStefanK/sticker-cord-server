cf63845fd5a102c5724ec236b390eab63be7bf22
Daniel Klose <danielklose@Daniels-MBP.fritz.box>
2020-04-18 15:06:39 +0200

    added file download ability

--- src/controllers/ImageController.ts
+++ src/controllers/ImageController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express'
 import { Controller, Get } from '@overnightjs/core'
 import { Logger } from '@overnightjs/logger'
 
+import * as mime from 'mime-types'
+
 import { Image, getConnection } from '../db'
 import * as ErrorHandler from './ErrorHandler'
 
@@ -10,15 +12,23 @@ class ImageController {
   @Get(':id')
   private async getPictureById(req: Request, res: Response): Promise<void> {
     const id = req.params.id
+    const filename = req.query.filename
     Logger.Info(`requesting image with id ${id}`)
     const c = getConnection()
     const imgRepo = c.getRepository(Image)
     imgRepo
       .findOne(id)
       .then((img) => {
+        const fileExtension = mime.extension(img?.mimetype || '')
+
         res.writeHead(200, {
           'Content-Type': img?.mimetype,
           'Content-Length': img?.data.byteLength,
+          ...(filename
+            ? {
+                'Content-disposition': `attachment; filename=${filename}.${fileExtension};`,
+              }
+            : {}),
         })
 
         res.end(img?.data)
