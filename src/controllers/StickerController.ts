0817ef4c21a75c5539c13525ccbc8748851276e1
Daniel Klose <danielklose@Daniels-MBP.fritz.box>
2020-04-18 19:45:57 +0200

    filter sticker

--- src/controllers/StickerController.ts
+++ src/controllers/StickerController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { Controller, Get } from '@overnightjs/core'
+import { Controller, Get, Post } from '@overnightjs/core'
 import { Logger } from '@overnightjs/logger'
 
 import { Sticker, getConnection } from '../db'
@@ -7,25 +7,47 @@ import * as ErrorHandler from './ErrorHandler'
 
 @Controller('sticker')
 class StickerController {
-  @Get('load/all')
+  @Post('load/all')
   private async getSticker(req: Request, res: Response): Promise<void> {
     Logger.Info(`requesting all tags`)
+    const { term, tags } = req.body
     const c = getConnection()
     const stickerRepo = c.getRepository(Sticker)
 
-    stickerRepo
-      .find({ relations: ['tags'] })
-      .then((s) => {
-        res.json({
-          success: true,
-          data: s,
+    try {
+      Logger.Info(tags)
+
+      const q = stickerRepo
+        .createQueryBuilder('sticker')
+        .leftJoinAndSelect('sticker.tags', 'tags')
+
+      if (tags && tags.length) {
+        q.where('tags.id in (:...tags)', {
+          tags: tags,
         })
-      })
-      .catch((e) => {
-        Logger.Err('could not load all sticker')
-        Logger.Err(e)
-        res.json(ErrorHandler.logProcessingError({}, 'load-sticker'))
-      })
+      }
+
+      if (term) {
+        q.andWhere('sticker.stickerName like :term', {
+          term: `${term}%`,
+        })
+      }
+
+      q.getMany()
+        .then((s) => {
+          res.json({
+            success: true,
+            data: s,
+          })
+        })
+        .catch((e) => {
+          Logger.Err('could not load all sticker')
+          Logger.Err(e)
+          res.json(ErrorHandler.logProcessingError({}, 'load-sticker'))
+        })
+    } catch (e) {
+      Logger.Err(e)
+    }
   }
 }
 
